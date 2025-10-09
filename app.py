b0446eb425021228e751fb5d72a6faaaa956a30f
AdrianAlexisMonrealRosas <24308060610026@cetis61.edu.mx>
2025-10-09 01:09:49 -0600

    aaaaa

---
+++ app.py
@@ -0,0 +1,26 @@
+from flask import Flask, render_template
+
+app = Flask(__name__)
+
+@app.route('/')
+def inicio():
+    return render_template('index.html', active_page='inicio')
+
+@app.route('/animales')
+def animales():
+    return render_template('animales.html', active_page='animales')
+
+@app.route('/vehiculos')
+def vehiculos():
+    return render_template('vehiculos.html', active_page='vehiculos')
+
+@app.route('/maravillas')
+def maravillas():
+    return render_template('maravillas.html', active_page='maravillas')
+
+@app.route('/acerca')
+def acerca():
+    return render_template('acerca.html', active_page='acerca')
+
+if __name__ == '__main__':
+    app.run(debug=True)
